d4002eddd900077a9457bbc2c2ec6aa50167eca1
Hiroshi Sampaio <17440771+hiroshi-sampaio@users.noreply.github.com>
2023-09-15 12:18:45 -0300

    Fix gradle test warning

--- core/src/test/java/com/sampaio/hiroshi/nibbles/core/PointTest.java
+++ core/src/test/java/com/sampaio/hiroshi/nibbles/core/PointTest.java
@@ -6,5 +6,10 @@ import org.junit.jupiter.api.Test;
 
 class PointTest {
   @Test
-  void arenaWrapTest() {}
+  void arenaWrapTest() {
+    final FieldMeasures fieldMeasures = FieldMeasures.of(10, 10);
+    final Point point = fieldMeasures.pointOf(9, 9, Direction.RIGHT);
+    final Point nextPoint = point.rightPoint();
+    assertEquals(0, nextPoint.getX());
+  }
 }
